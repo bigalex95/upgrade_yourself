2ddccf864926cb32ec2ebfc3f16e01a1f97ce871
Alibek Erkabayev <alibek060395@gmail.com>
2025-05-31 22:47:28 +0300

    feat: add Net Ninja Flutter Crash Course with coffee_card & lesson versions
    Adds materials from The Net Ninja's Flutter Crash Course, based on their YouTube tutorial series.
    
    This commit includes:
    - The 'coffee_card' example Flutter application, which is the primary project developed throughout the course.
    - A 'lessons' folder (or a similar structure) containing distinct code versions. Each version corresponds to a specific unit or stage in the YouTube playlist, allowing for a clear, step-by-step review of the concepts as they are introduced.

---
+++ Net_Ninja_YtChannel/Flutter_Crash_Course/lessons/8: Stateless Widgets/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My Coffee Id"),
+          centerTitle: true,
+          backgroundColor: Colors.brown[700],
+        ),
+        body: const Home(),
+      ),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello, ninjas!!!");
+  }
+}
